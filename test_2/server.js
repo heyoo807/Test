11e5736ac94c16e028c2c1275f8dfc8bd1f227f7
heyoo807 <yoohaeun2002@naver.com>
2023-07-26 22:15:23 +0900

    CustomerAdd-insert

--- test_2/server.js
+++ test_2/server.js
@@ -21,6 +21,9 @@ const connection = mysql.createConnection({
 }) 
 connection.connect()
 
+const multer = require('multer')
+const upload = multer({dest: './upload'})
+
 app.get('/api/customers', (req, res)=>{
     connection.query(
       "SELECT * FROM CUSTOMER",
@@ -28,6 +31,17 @@ app.get('/api/customers', (req, res)=>{
         res.send(rows)
       }
     )
+})
+
+app.use('/image', express.static('./upload'))
+app.post('/api/customers', upload.single('image'), (req, res) => {
+  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)'
+  let image = '/image/' + req.file.filename;
+  let name = req.body.name
+  let name = req.body.name
+  let name = req.body.name
+  let name = req.body.name
+  let name = req.body.name
 })
 
 app.listen(port, ()=> console.log(`listening on port ${port}`))
